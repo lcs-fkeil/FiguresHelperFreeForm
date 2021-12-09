ac4527e5abe06c5e6a5e6828ab923e026efe0d1c
Franka Keil <fkeil@lcs.on.ca>
2021-12-08 19:46:17 -0500

    Added all the changes made in class. More detail on Edsby.

--- FiguresHelperFreeForm/Views/2D Shapes/CircleView.swift
+++ FiguresHelperFreeForm/Views/2D Shapes/CircleView.swift
@@ -10,10 +10,45 @@ import SwiftUI
 struct CircleView: View {
     
     // MARK: Stored properties
-    @State var radius = 10.0
+    // Whatever the user adds to the text field
+    @State var providedRadius = ""
+    
     
     // MARK: Computed properties
-    var area: Double {
+    // Check the input given by the user
+    // If possible, return a Double
+    // Otherwise (bad input) return nil
+    var radius: Double? {
+        
+        // Tests of the provided input
+        // 1. Ensure that we can simply change the input into a Double
+        // 2. Ensure that the value as a Double is more than 0
+        // With a "guard" statement, we list the things we wish to be
+        // true... and provide an action to carry out when those
+        // conditions are not met.
+        guard let radius = Double(providedRadius),
+              radius > 0
+        else {
+            // When the tests are failed, we do not have a valid radius
+            return nil
+        }
+        
+        // If we get here, we know the radius is good
+        return radius
+        
+    }
+    
+    
+    // Attempts to calculate the area, if it can't... returns nil
+    var area: Double? {
+        
+        // Is the input actually a Double, or... is it Double? (might be nil)
+        guard let radius = radius
+        else {
+            // We didn't have a valid radius
+            // We can't calcualte the area...
+            return nil
+        }
         return Double.pi * radius * radius
     }
     
@@ -26,23 +61,16 @@ struct CircleView: View {
                             horizontalPadding: 50)
                 
                 SectionLabelView(text: "Radius", variable: "r")
-
-                // Input: Radius
-                Slider(value: $radius,
-                       in: 0.0...100.0,
-                       step: 0.1,
-                       label: {
-                    Text("Radius")
-                },
-                       minimumValueLabel: {
-                    Text("0")
-                },
-                       maximumValueLabel: {
-                    Text("100")
-                })
                 
-                // Output: Radius
-                SliderValueView(value: radius)
+                // Input: Radius
+                TextField("Radius",
+                          text: $providedRadius,
+                          prompt: Text("Numeric value greater than 0"))
+                //                    Ternary conditional operator
+                //                    "One line if statement"
+                //                    CONDITION        TRUE         FALSE
+                    .foregroundColor(radius == nil ? Color.red : Color.primary)
+                
                 
                 SectionLabelView(text: "Area", variable: "")
                 
